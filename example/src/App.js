dde0f9824bc70e15a3479dad7b4029230408988b
詹雅雯MIKA <mika@mitac.com.tw>
2022-03-28 17:28:34 +0800

    設置Router

--- example/src/App.js
+++ example/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from "react";
+import { BrowserRouter, Routes } from 'react-router-dom'; 
+import {HashRouter,Route} from "react-router-dom"
+import index from './pages/index.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="indexWidth">
+        <BrowserRouter>
+          <HashRouter>
+            <Routes>
+              <Route exact={true} path="/pages/index.js" component={index} /> 
+              
+            </Routes>
+          </HashRouter>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
